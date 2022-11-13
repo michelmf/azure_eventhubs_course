da843391d351b07bff422f4174da799b345e05c1
Michel Morais Ferreira <michelmf@poli.ufrj.br>
2022-11-13 20:47:45 -0300

    Add output variables

--- challenge_1/terraform/infra/output.tf
+++ challenge_1/terraform/infra/output.tf
@@ -2,18 +2,22 @@
     Output Information
 */
 
+output "resource_group_name" {
+  value = "Resource Group name: ${azurerm_resource_group.rg_challenge.name}"
+}
+
 output "eventhubs_namespace" {
-  value = " "
+  value = "Eventhubs Namespace name: ${azurerm_eventhub_namespace.evhn_challenge.name}"
+}
+
+output "eventhubs__namespace_throughput_unit" {
+  value = "#Throughput Units: ${azurerm_eventhub_namespace.evhn_challenge.capacity}"
+}
+
+output "eventhubs_namespace_pricing_tier" {
+  value = "SKU: ${azurerm_eventhub_namespace.evhn_challenge.sku}"
 }
 
 output "eventhubs_eventhub" {
-  value = ""
-}
-
-output "eventhubs_throughput_unit" {
-  value = ""
-}
-
-output "eventhubs_pricing_tier" {
-  value = ""
+  value = "Event hub name: ${azurerm_eventhub.eventhub.name}"
 }
